be0bb620894012eecc9fb8ba3dd3028dd9e1c754
wp360 <2897406527@qq.com>
2018-07-08 00:10:19 +0800

    add file

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <footer-guide></footer-guide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+
+export default {
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
