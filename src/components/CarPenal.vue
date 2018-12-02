be2e0f4742b2191ed3c940613bdb54316d8a1f34
wp360 <2897406527@qq.com>
2018-12-03 00:52:47 +0800

    购物车悬停显示

--- src/components/CarPenal.vue
+++ src/components/CarPenal.vue
@@ -1,11 +1,11 @@
 <template>
-  <li class="nav-cart">
+  <li class="nav-cart" @mouseenter="showCarHandle" @mouseleave="hideCarHandle">
     <a href="javascript:;">购物车</a>
     <!--根据class改变颜色-->
     <span class="cart-empty-num" :class="{'cart-num':totalCount>0}">
       <i>{{totalCount}}</i>
     </span>
-    <div class="nav-cart-wrapper">
+    <div class="nav-cart-wrapper" v-if="carShow">
       <div class="nav-cart-list">
         <div class="empty" v-if="totalCount<=0">
           <h3>购物车为空</h3>
@@ -63,11 +63,20 @@
       },
       totalPrice () {
         return this.$store.getters.totalPrice
+      },
+      carShow () {
+        return this.$store.state.carShow
       }
     },
     methods: {
       delCarPanelHandle (id) {
         this.$store.commit('delCarPanelData', id)
+      },
+      showCarHandle () {
+        this.$store.commit('showCar')
+      },
+      hideCarHandle () {
+        this.$store.commit('hideCar')
       }
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,9 @@ Vue.use(Vuex)
 let store = new Vuex.Store({
   state: {
     carPanelData: [],
-    maxOff: false
+    maxOff: false,
+    carShow: false,
+    carTimer: null
   },
   getters: {
     totalCount (state) {
@@ -34,13 +36,16 @@ let store = new Vuex.Store({
           if (goods.count > goods.limit_num) {
             goods.count --
             state.maxOff = true
+            return
           }
+          state.carShow = true
         }
       })
       if(bOff) {
         let goodsData = data
         Vue.set(goodsData,'count',1)
         state.carPanelData.push(goodsData)
+        state.carShow = true
       }
       console.log(state.carPanelData)
     },
@@ -54,6 +59,15 @@ let store = new Vuex.Store({
     },
     closePrompt (state) {
       state.maxOff = false
+    },
+    showCar (state) {
+      clearTimeout(state.carTimer)
+      state.carShow = true
+    },
+    hideCar (state) {
+      state.carTimer = setTimeout(() => {
+        state.carShow = false
+      }, 500)
     }
   }
 })
