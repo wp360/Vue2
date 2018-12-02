755f1113155f41eb531038d96162e0caf5a35ef4
wp360 <2897406527@qq.com>
2018-12-02 18:35:32 +0800

    add file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header-nav></header-nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HeaderNav from './components/HeaderNav.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HeaderNav
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import '@/assets/css/reset.css'
+import '@/assets/css/header.css'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: []
+})
