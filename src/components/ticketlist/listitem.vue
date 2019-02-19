bf4c0a11b332ff8313e5ba1f803b3b22ffba412e
lztyytzl <1156258101@qq.com>
2019-02-19 10:05:52 +0800

    props使用

---
+++ src/components/ticketlist/listitem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list">
+        <ul>
+            <li @click="handleClick()">这是第一条数据</li>
+        </ul>
+        <!-- 在页面中把接收的数据显示出来 -->
+        <!-- <p>{{message}}</p> -->
+        <p>{{receive}}</p>
+    </div>
+</template>
+<script>
+export default {
+  props: ['message'],
+  data () {
+    return {
+      receive: this.message
+    }
+  },
+  methods: {
+    handleClick () {
+      console.log('点击')
+    }
+  }
+}
+</script>
+<style scoped>
+    .list {
+        color: #f00;
+    }
+    .list p {
+        font-weight: 700;
+    }
+</style>
--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,15 +1,37 @@
 <template>
     <div class='banner'>
         <h3>用户页面</h3>
+        <div class="page">
+          <h3>这是列表页</h3>
+          <button class="btn" @click="skip()">跳转到详情页</button>
+          <button class="btn" @click="changeData()">改变数据</button>
+          <!-- 组件调用 来自列表页的数据 -->
+          <!-- <ListItem :message="listdata"></ListItem> -->
+          <ListItem :message="count"></ListItem>
+        </div>
     </div>
 </template>
 
 <script>
+// 引入组件
+import ListItem from '@/components/ticketlist/listitem'
 export default {
   data () {
-    return {}
+    return {
+      // listdata: '',
+      count: 100
+    }
   },
-  components: {}
+  components: {
+    // 注册组件
+    ListItem
+  },
+  methods: {
+    // 单击按钮改变listdata数据
+    changeData () {
+      this.listdata = '单击按钮之后的数据'
+    }
+  }
 }
 </script>
 
