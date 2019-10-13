1959b1dcb4fea414ce8f42bdf8fb4052b0732e10
wp360 <2897406527@qq.com>
2019-10-13 23:14:22 +0800

    注册页面

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -5,6 +5,29 @@
         <span class="title">
           Vue在线后台管理系统
         </span>
+        <el-form :model="registerUser" :rules="rules" ref="registerForm" label-width="80px" class="registerForm">
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="registerUser.email" placeholder="请输入邮箱地址"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="registerUser.password" placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="password2">
+            <el-input type="password" v-model="registerUser.password2" placeholder="请确认密码"></el-input>
+          </el-form-item>
+          <el-form-item label="选择身份">
+            <el-select v-model="registerUser.identity" placeholder="请选择身份">
+              <el-option label="管理员" value="manager"></el-option>
+              <el-option label="员工" value="employee"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">注册</el-button>
+          </el-form-item>
+        </el-form>
       </div>
     </section>
   </div>
@@ -12,8 +35,71 @@
 <script>
 export default {
   name: 'register',
-  components: {
-
+  components: {},
+  data () {
+    var validatePass2 = (rule, value, callback) => {
+      if (value !== this.registerUser.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      registerUser: {
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        identity: ''
+      },
+      rules: {
+        name: [
+          {
+            required: true, message: '用户名不能为空', trigger: 'blur'
+          },
+          {
+            min: 2,
+            max: 30,
+            message: '长度在2到30个字符之间',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            type: 'email',
+            required: true,
+            message: '邮箱格式不正确',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 30, message: '长度在6到30个字符', trigger: 'blur' }
+        ],
+        password2: [
+          { required: true, message: '确认密码不能为空', trigger: 'blur' },
+          {
+            min: 6,
+            max: 30,
+            message: '长度在6到30个字符',
+            trigger: 'blur'
+          },
+          { validator: validatePass2, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('注册成功！')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
   }
 }
 </script>
@@ -37,7 +123,7 @@ export default {
   text-align: center;
 }
 .form_container .manage_tip .title {
-  font-family: "Microsoft YaHei";
+  font-family:"Microsoft YaHei";
   font-weight: bold;
   font-size: 26px;
   color: #fff;
