abebc9892eddfd0db60bc4eccf400b0e919d776e
wp360 <2897406527@qq.com>
2020-05-02 10:39:20 +0800

    字符串使用

--- src/components/list.vue
+++ src/components/list.vue
@@ -2,9 +2,12 @@
   <div class="list">
     <ul>
       <li v-for="item in list" :key="item.name">
+        <!-- {{ item.name.replace(/-/g, '') }} 使用正则表达式去除“-” -->
+        <!-- {{ item.name.trim() }} 使用trim()去除空格  -->
         {{ item.name }} —— {{ item.addr }}
       </li>
     </ul>
+    {{ priceTxt }}
   </div>
 </template>
 <script>
@@ -13,7 +16,8 @@ export default {
   name: 'list',
   data () {
     return {
-      list: []
+      list: [],
+      priceTxt: ''
     }
   },
   async mounted () {
@@ -21,8 +25,9 @@ export default {
     // if (data && data.code === 0) {
     //   this.list = data.list
     // }
-    const { data: { list } } = await axios.get('/list')
+    const { data: { list, price, rate } } = await axios.get('/list')
     this.list = list
+    this.priceTxt = navigator.language === 'zh-CN' ? `￥${price * rate}` : `$${price}`
   }
 }
 </script>
